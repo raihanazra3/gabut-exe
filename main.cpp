0a7a0606ac5a0f679cb4afc2c622bfec7f7fb2bb
Sandy Kalamuddin <sandykalamuddin07@gmail.com>
2020-05-11 15:58:07 +0700

    contoh init
    Co-authored-by: Raihan Azra <raihanazra3@hotmail.com>

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// todo
+// mau buat apa kita?
+
+// saya coba buat menu dulu kali ya?
+
+#include <iostream>
+
+using namespace std;
+
+// void
+void hello()
+{
+}
+
+int main()
+{
+    cout << "Hello Wordl";
+
+    cin.get();
+    return 0;
+    // coba buat hello world sederhana
+    // ini ga ada masalah kan?
+    // sekarang coba kita konek ke github ente zra
+    // ane coba pull project nembak yang ada di github ente
+    // oke
+    /* cara nya gimana tuh sen? , btw sen beda nya void main sama int main apa ? 
+       saya dari kemarin bignung beda nya void main sama int main bisa jelasin dikit?*/
+}
